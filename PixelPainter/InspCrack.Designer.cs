5ee6caa02781e0cdf2eef91b3a703c4fffde1fe2
ykPark0923 <puklove22@naver.com>
2025-03-24 17:45:53 +0900

    feat: 크랙 검출, align, diff 일부 구현

--- PixelPainter/InspCrack.Designer.cs
+++ PixelPainter/InspCrack.Designer.cs
@@ -29,13 +29,17 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.openBTN = new System.Windows.Forms.Button();
+            this.openBTN1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
+            this.contourBTN = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openBTN2 = new System.Windows.Forms.Button();
+            this.diffBTN = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -47,15 +51,15 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // openBTN
+            // openBTN1
             // 
-            this.openBTN.Location = new System.Drawing.Point(49, 592);
-            this.openBTN.Name = "openBTN";
-            this.openBTN.Size = new System.Drawing.Size(74, 32);
-            this.openBTN.TabIndex = 2;
-            this.openBTN.Text = "열기";
-            this.openBTN.UseVisualStyleBackColor = true;
-            this.openBTN.Click += new System.EventHandler(this.openBTN_Click);
+            this.openBTN1.Location = new System.Drawing.Point(49, 592);
+            this.openBTN1.Name = "openBTN1";
+            this.openBTN1.Size = new System.Drawing.Size(74, 32);
+            this.openBTN1.TabIndex = 2;
+            this.openBTN1.Text = "열기";
+            this.openBTN1.UseVisualStyleBackColor = true;
+            this.openBTN1.Click += new System.EventHandler(this.openBTN_Click);
             // 
             // openFileDialog1
             // 
@@ -65,35 +69,68 @@
             // 
             this.openFileDialog2.FileName = "openFileDialog2";
             // 
-            // button1
+            // contourBTN
             // 
-            this.button1.Location = new System.Drawing.Point(150, 592);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 32);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "검사";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.contourBTN.Location = new System.Drawing.Point(357, 588);
+            this.contourBTN.Name = "contourBTN";
+            this.contourBTN.Size = new System.Drawing.Size(74, 32);
+            this.contourBTN.TabIndex = 6;
+            this.contourBTN.Text = "검사";
+            this.contourBTN.UseVisualStyleBackColor = true;
+            this.contourBTN.Click += new System.EventHandler(this.contourBTN_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(352, 592);
+            this.textBox1.Location = new System.Drawing.Point(451, 588);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 28);
             this.textBox1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(589, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(572, 550);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openBTN2
+            // 
+            this.openBTN2.Location = new System.Drawing.Point(611, 588);
+            this.openBTN2.Name = "openBTN2";
+            this.openBTN2.Size = new System.Drawing.Size(74, 32);
+            this.openBTN2.TabIndex = 9;
+            this.openBTN2.Text = "열기";
+            this.openBTN2.UseVisualStyleBackColor = true;
+            this.openBTN2.Click += new System.EventHandler(this.openBTN2_Click);
+            // 
+            // diffBTN
+            // 
+            this.diffBTN.Location = new System.Drawing.Point(1087, 584);
+            this.diffBTN.Name = "diffBTN";
+            this.diffBTN.Size = new System.Drawing.Size(74, 32);
+            this.diffBTN.TabIndex = 10;
+            this.diffBTN.Text = "검사";
+            this.diffBTN.UseVisualStyleBackColor = true;
+            this.diffBTN.Click += new System.EventHandler(this.diffBTN_Click);
             // 
             // InspCrack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 752);
+            this.ClientSize = new System.Drawing.Size(1368, 752);
+            this.Controls.Add(this.diffBTN);
+            this.Controls.Add(this.openBTN2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.openBTN);
+            this.Controls.Add(this.contourBTN);
+            this.Controls.Add(this.openBTN1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "InspCrack";
             this.Text = "editIMG";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,11 +139,14 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button openBTN;
+        private System.Windows.Forms.Button openBTN1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.OpenFileDialog openFileDialog2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button contourBTN;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button openBTN2;
+        private System.Windows.Forms.Button diffBTN;
     }
 }
--- PixelPainter/InspCrack.cs
+++ PixelPainter/InspCrack.cs
@@ -9,25 +9,36 @@ namespace PixelPainter
 {
     public partial class InspCrack : Form
     {
-        private bool  _IsCracked;
-
+        private bool _IsCracked;
         public InspCrack()
         {
             InitializeComponent();
         }
 
         private Mat src = new Mat();
+        private Mat src2 = new Mat();
+        private Mat aligned1 = new Mat();
+        private Mat aligned2 = new Mat();
+        private Mat diffImage = new Mat();
 
         private void openBTN_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 src = Cv2.ImRead(openFileDialog1.FileName);
+                src = Cv2.ImRead(openFileDialog1.FileName);
                 pictureBox1.Image = BitmapConverter.ToBitmap(src);
             }
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void openBTN2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                src2 = Cv2.ImRead(openFileDialog2.FileName);
+                pictureBox2.Image = BitmapConverter.ToBitmap(src2);
+            }
+        }
+        private void contourBTN_Click(object sender, EventArgs e)
         {
             if (src.Empty())
             {
@@ -35,7 +46,9 @@ namespace PixelPainter
                 return;
             }
 
-            // 크랙 검사
+            // 크랙 검사 HoughLine
+            //if (IsPCBCracked(src))
+
             if (IsPCBCracked(src))
             {
                 textBox1.Text = "NG: Crack"; // 크랙이 감지되면 NG 출력
@@ -45,7 +58,10 @@ namespace PixelPainter
                 textBox1.Text = "OK"; // 정상이면 OK 출력
             }
         }
-
+        private void diffBTN_Click(object sender, EventArgs e)
+        {
+            AlignAndCompare();
+        }
         private bool IsPCBCracked(Mat src)
         {
             Mat gray = new Mat();
@@ -91,17 +107,132 @@ namespace PixelPainter
 
                 pictureBox1.Image = BitmapConverter.ToBitmap(dst);
 
-                if (perimeter > 2148 || perimeter < 2091)
+                if (perimeter <= 2150 && perimeter >= 2090)
                 {
-                    _IsCracked = true;  //크랙있음
+                    _IsCracked = false;  //크랙없음
                 }
                 else
                 {
-                    _IsCracked = false;  //크랙없음
+                    _IsCracked = true;  //크랙있음
                 }
             }
             return _IsCracked;
         }
+        private void AlignAndCompare()
+        {
+            if (src.Empty() || src2.Empty())
+            {
+                MessageBox.Show("이미지를 먼저 로드하세요.");
+                return;
+            }
 
+            // PCB 정렬
+            aligned1 = AlignPCB(src);
+            aligned2 = AlignPCB(src2);
+
+            // 정렬된 이미지 표시
+            Cv2.ImShow("Aligned Image 1", aligned1);
+            Cv2.ImShow("Aligned Image 2", aligned2);
+
+            // 크기 맞추기 (정렬 후 이미지 크기가 다를 경우 대비)
+            //if (aligned1.Size() != aligned2.Size())
+            //{
+            //    Cv2.Resize(aligned2, aligned2, aligned1.Size());
+            //}
+
+            // 이미지 비교 (Bitwise XOR 활용)
+            diffImage = new Mat();
+            Cv2.Absdiff(aligned1, aligned2, diffImage);
+
+            // 비교 결과 표시
+            Cv2.ImShow("Difference", diffImage);
+            //pictureBox2.Image = BitmapConverter.ToBitmap(diffImage);
+
+            Cv2.WaitKey(0);
+        }
+        private Point2f[] OrderPoints(Point[] points)
+        {
+            Point2f[] ordered = new Point2f[4];
+
+            // X 좌표 기준으로 정렬
+            var sortedX = points.OrderBy(p => p.X).ToArray();
+            var left = sortedX.Take(2).OrderBy(p => p.Y).ToArray();  // 좌측 상/하
+            var right = sortedX.Skip(2).OrderBy(p => p.Y).ToArray(); // 우측 상/하
+
+            ordered[0] = left[0];  // 좌상단
+            ordered[1] = right[0]; // 우상단
+            ordered[2] = right[1]; // 우하단
+            ordered[3] = left[1];  // 좌하단
+
+            return ordered;
+        }
+        private Mat AlignPCB(Mat src)
+        {
+            Mat gray = new Mat();
+            Mat binary = new Mat();
+            Mat result = new Mat();
+
+            // 1. 그레이스케일 변환
+            Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+
+            // 2. 적응형 이진화 OR Otsu 이진화 (조명 상태에 따라 선택)
+            Cv2.AdaptiveThreshold(gray, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, 11, 2);
+            // Cv2.Threshold(gray, binary, 0, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);  // Otsu 이진화 사용 가능
+
+            // 3. 컨투어 검출
+            Point[][] contours;
+            HierarchyIndex[] hierarchy;
+            Cv2.FindContours(binary, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            // 4. 가장 큰 컨투어 찾기 (면적 기준)
+            Point[] largestContour = contours.OrderByDescending(c => Cv2.ContourArea(c)).FirstOrDefault();
+            if (largestContour == null || Cv2.ContourArea(largestContour) < 100) return src;
+
+            // 5. 컨투어 근사화하여 4개 점 찾기
+            Point[] approx = Cv2.ApproxPolyDP(largestContour, 0.02 * Cv2.ArcLength(largestContour, true), true);
+
+            // 6. Convex Hull 보정 후 다시 시도
+            if (approx.Length != 4)
+            {
+                approx = Cv2.ConvexHull(approx);
+            }
+
+            // 7. 여전히 4개가 아니라면 바운딩 박스 사용
+            if (approx.Length != 4)
+            {
+                Console.WriteLine("Crack");
+                Rect boundingBox = Cv2.BoundingRect(largestContour);
+                approx = new Point[]
+                {
+            new Point(boundingBox.Left, boundingBox.Top),
+            new Point(boundingBox.Right, boundingBox.Top),
+            new Point(boundingBox.Right, boundingBox.Bottom),
+            new Point(boundingBox.Left, boundingBox.Bottom)
+                };
+            }
+
+            // 8. 투시 변환 적용
+            if (approx.Length == 4)
+            {
+                Point2f[] srcPoints = OrderPoints(approx);
+                Point2f[] dstPoints =
+                {
+            new Point2f(0, 0),
+            new Point2f(src.Cols - 1, 0),
+            new Point2f(src.Cols - 1, src.Rows - 1),
+            new Point2f(0, src.Rows - 1)
+        };
+
+                Mat perspectiveMatrix = Cv2.GetPerspectiveTransform(srcPoints, dstPoints);
+                Cv2.WarpPerspective(src, result, perspectiveMatrix, src.Size());
+
+                return result;
+            }
+            else
+            {
+                MessageBox.Show("정확한 꼭지점을 찾을 수 없어 원본 이미지를 반환합니다.");
+                return src;
+            }
+        }        
     }
 }
