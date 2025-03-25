eb8a200a8a0aa793360755ad409f55041fc0af62
Zephyros12 <rimrbtjd12@gmail.com>
2025-03-25 15:08:58 +0900

    스크래치 검출 초기버전 아직 스크래치를 잘 못 감지함

--- PixelPainter/InspScratch.Designer.cs
+++ PixelPainter/InspScratch.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PixelPainter
 {
-    partial class InspCrack
+    partial class InspScratch
     {
         /// <summary>
         /// Required designer variable.
@@ -29,50 +29,23 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.openBTN1 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openBTN = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.diffBTN = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(571, 550);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // openBTN1
-            // 
-            this.openBTN1.Location = new System.Drawing.Point(49, 592);
-            this.openBTN1.Name = "openBTN1";
-            this.openBTN1.Size = new System.Drawing.Size(74, 32);
-            this.openBTN1.TabIndex = 2;
-            this.openBTN1.Text = "열기";
-            this.openBTN1.UseVisualStyleBackColor = true;
-            this.openBTN1.Click += new System.EventHandler(this.openBTN_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // openFileDialog2
-            // 
-            this.openFileDialog2.FileName = "openFileDialog2";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(451, 588);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 28);
-            this.textBox1.TabIndex = 7;
             // 
             // pictureBox2
             // 
@@ -80,31 +53,52 @@
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(571, 550);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            // 
+            // openBTN
+            // 
+            this.openBTN.Location = new System.Drawing.Point(12, 568);
+            this.openBTN.Name = "openBTN";
+            this.openBTN.Size = new System.Drawing.Size(74, 32);
+            this.openBTN.TabIndex = 3;
+            this.openBTN.Text = "열기";
+            this.openBTN.UseVisualStyleBackColor = true;
+            this.openBTN.Click += new System.EventHandler(this.openBTN_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(537, 572);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 8;
             // 
             // diffBTN
             // 
-            this.diffBTN.Location = new System.Drawing.Point(1087, 584);
+            this.diffBTN.Location = new System.Drawing.Point(1086, 572);
             this.diffBTN.Name = "diffBTN";
             this.diffBTN.Size = new System.Drawing.Size(74, 32);
-            this.diffBTN.TabIndex = 10;
+            this.diffBTN.TabIndex = 11;
             this.diffBTN.Text = "검사";
             this.diffBTN.UseVisualStyleBackColor = true;
             this.diffBTN.Click += new System.EventHandler(this.diffBTN_Click);
             // 
-            // InspCrack
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // InspScratch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1369, 752);
             this.Controls.Add(this.diffBTN);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.openBTN1);
+            this.Controls.Add(this.openBTN);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Name = "InspCrack";
-            this.Text = "editIMG";
+            this.Name = "InspScratch";
+            this.Text = "InspScratch";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
@@ -115,12 +109,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button openBTN1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button openBTN;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button diffBTN;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
---
+++ PixelPainter/InspScratch.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace PixelPainter
+{
+    public partial class InspScratch : Form
+    {
+
+        private readonly string normalImagePath = @"Image.bmp";
+
+        public InspScratch()
+        {
+            InitializeComponent();
+
+            // 정상이미지를 경로로 고정
+            src2 = Cv2.ImRead(normalImagePath);
+            if (src2.Empty()) return;
+
+            pictureBox2.Image = BitmapConverter.ToBitmap(src2);
+        }
+
+        private Mat src = new Mat();
+        private Mat src2 = new Mat();
+        private Mat diffImage = new Mat();
+
+        private void openBTN_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                src = Cv2.ImRead(openFileDialog1.FileName);
+                pictureBox1.Image = BitmapConverter.ToBitmap(src);
+            }
+        }
+
+        private void diffBTN_Click(object sender, EventArgs e)
+        {
+            DiffCompare();
+        }
+        private void DiffCompare()
+        {
+            if (src.Empty() || src2.Empty())
+            {
+                Console.WriteLine("이미지를 먼저 로드하세요.");
+                return;
+            }
+
+            diffImage = new Mat();
+            Cv2.Absdiff(src, src2, diffImage);
+
+            detectScratch();
+        }
+
+        private void detectScratch()
+        {
+            // 그레이스케일 변환
+            Mat grayDiff = new Mat();
+            Cv2.CvtColor(diffImage, grayDiff, ColorConversionCodes.BGR2GRAY);
+
+            // 이진화 (Threshold 적용)
+            Mat binaryDiff = new Mat();
+            Cv2.Threshold(grayDiff, binaryDiff, 50, 255, ThresholdTypes.Binary);
+
+            // 윤곽선 검출
+            Point[][] contours;
+            HierarchyIndex[] hierarchy;
+            Cv2.FindContours(binaryDiff, out contours, out hierarchy, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+
+            bool scratchDetected = false;
+            Mat resultImage = src.Clone();
+
+            foreach (var contour in contours)
+            {
+                double area = Cv2.ContourArea(contour);
+                if (area > 10 && area < 500)  // 일정 크기 범위 내에서 스크래치만 감지
+                {
+                    // 윤곽선의 경계 직선화 (스크래치 형태는 직선으로 간주)
+                    RotatedRect box = Cv2.MinAreaRect(contour);
+                    float aspectRatio = Math.Max(box.Size.Width, box.Size.Height) / Math.Min(box.Size.Width, box.Size.Height);
+
+                    // 직선처럼 긴 형태만 스크래치로 감지
+                    if (aspectRatio > 5)  // 길이가 넓이보다 5배 이상일 때 직선 형태로 인식
+                    {
+                        Rect boundingBox = Cv2.BoundingRect(contour);
+                        Cv2.Rectangle(resultImage, boundingBox, new Scalar(0, 0, 255), 2); // 빨간색 박스 그리기
+                        scratchDetected = true;
+                    }
+                }
+            }
+
+            // 결과 표시
+            pictureBox1.Image = BitmapConverter.ToBitmap(resultImage);
+
+            // 스크래치가 감지되었다면 NG 처리
+            textBox1.Text = scratchDetected ? "NG: Scratch" : "OK";
+        }
+    }
+}
